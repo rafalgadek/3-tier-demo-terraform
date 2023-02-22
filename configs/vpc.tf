3a2c5f49d3ebb09ef30a10d1078ad413c4145bc1
Raf <rafalgadek@gmail.com>
2023-02-22 13:41:59 +0100

    add .gitignore

--- configs/vpc.tf
+++ configs/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "rg_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-tags = {
-  Name = "RG VPC"
-}
+  tags = {
+    Name = "RG VPC"
+  }
 }
